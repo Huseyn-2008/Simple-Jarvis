9e01036338c088a93a527e1a480a5463aa1c2047
Only Up <76998342+Huseyn-2008@users.noreply.github.com>
2023-11-23 12:46:30 +0400

    Update vs.py
    Added something and Now it speaks male voice

--- vs.py
+++ vs.py
@@ -5,67 +5,140 @@ import webbrowser
 import os
 import random
 from time import sleep
-import pyautogui
 import winshell
-
+import wikipedia
+import subprocess
 
 
 start = pyttsx3.init()
 
-finish = ["всегда к  вашим услугам сэр", "сделано сэр", "готово", "все  сделано", "пожалуйста сэр"]
+voices = start.getProperty('voices')
+start.setProperty('voice', voices[2].id)
 
+finish = ["at your service, sir", "done, sir", "completed", "all done", "here you go, sir"]
 
 opts = {
-    "alias": ('джарвис','не спишь','ты тут','здравствуй','ты нужен мне'),
-    "tbr": ('скажи','расскажи','покажи','сколько','произнеси'),
-    "note": ('блокнот','открой блокнот'),
-    "calc": ('калькулятор', 'открой калькулятор'),
-    "sublime": ('открой редактор кода','открой редактор'),
-    "pixel": ('пиксельный редактор', 'открой пиксельный редактор', 'открой камни пиксельный редактор'),
-    "ctime": ('текущее время','сейчас времени','который час','который год'),
-    "radio": ('включи музыку','воспроизведи радио','включи радио'),
-    "stupid1": ('расскажи анекдот','рассмеши меня','ты знаешь анекдоты'),
-    "chrome": ('браузер', 'открой браузер'),
-    "bye": ('пока джарвис','спокойной ночи'),
-    "google": ('открой google'),
-    "camcam": ('камера', 'включи камеру', 'открой доступ к камере'),
-    "thank": ('спасибо'),
-    "bin": ('очистить корзину')
-
+    "alias": ('jarvis', 'are you there', 'hey', 'hello', 'you needed'),
+    "tbr": ('say', 'tell me', 'show me', 'how many', 'pronounce'),
+    "note": ('notepad', 'open notepad'),
+    "calc": ('calculator', 'open calculator'),
+    "sublime": ('open code editor', 'open code'),
+    "pixel": ('pixel editor', 'open pixel editor', 'open stones pixel editor'),
+    "ctime": ('current time', 'now', 'what time is it', 'what year is it'),
+    "radio": ('play music', 'play radio', 'turn on radio'),
+    "stupid1": ('tell a joke', 'make me laugh', 'you know any jokes'),
+    "chrome": ('browser', 'open browser'),
+    "bye": ('goodbye jarvis', 'good night'),
+    "google": ('open google'),
+    "camcam": ('camera', 'turn on camera', 'grant access to camera'),
+    "thank": ('thank you'),
+    "bin": ('empty recycle bin'),
+    "tell": ('tell me something', 'share an interesting fact', 'give me information'),
+    "how_are_you": ('how are you', 'how are you doing', 'what\'s up')
 }
 
+def custom_log(text):
+    print("[log]: " + text)
+    start.say(text)
+    start.runAndWait()
 
+def respond_to_need_help():
+    responses = ["Of course! I'm here to help.", 
+                 "Absolutely! What do you need assistance with?", 
+                 "I'm ready to assist you. What can I help you with today?"]
+    custom_log(random.choice(responses))
+
+def regular_speak_greeting():
+    greetings = ["Hello!", "Hi there!", "Greetings!", "Hey!"]
+    custom_log(random.choice(greetings))
+
+def regular_speak_response():
+    responses = ["Sure, what can I do for you?", "Of course!", "I'm here to help.", "How can I assist you today?"]
+    custom_log(random.choice(responses))
+
+def regular_speak_farewell():
+    farewells = ["Goodbye!", "See you later!", "Take care!", "Until next time!"]
+    custom_log(random.choice(farewells))
+
+def respond_to_how_are_you():
+    responses = ["I'm doing well, thank you!", "I'm functioning at optimal capacity!", "All systems are go!", "I'm great, sir!"]
+    custom_log(random.choice(responses))
+
+CHROME_PATH = r"C:\\Program Files\\Google\\Chrome\Application\\chrome.exe"
+
+def announce_opening(url):
+    url_without_https = url.replace("https://", "").replace("http://", "")
+    start.say(f"Opening {url_without_https} in Google Chrome")
+    start.runAndWait()
+
+def open_website(url):
+    try:
+        announce_opening(url)
+        subprocess.run([CHROME_PATH, url], check=True)
+    except Exception as e:
+        custom_log(f"Failed to open {url} in Google Chrome. Error: {e}")
+
+def respond_to_new_project():
+    responses = ["That's great! What kind of project are you thinking of starting?", 
+                 "Starting a new project is always exciting! What's the focus of your project?", 
+                 "Excellent! Tell me more about the project you want to start."]
+    custom_log(random.choice(responses))
 
 def listen():
     r = sr.Recognizer()
     r.pause_threshold = 0.5
     with sr.Microphone() as source:
+        print("Listening...")
         r.pause_threshold = 0.5
-        r.adjust_for_ambient_noise(source, duration= 0.5)
+        r.adjust_for_ambient_noise(source, duration=0.5)
         audio = r.listen(source)
         try:
-            task = r.recognize_google(audio, language="ru-RU").lower()
+            task = r.recognize_google(audio, language="en-US").lower()
             print("[log]: " + task)
         except:
             task = listen()
         return task
 
-
-
+def tell_me_something():
+    try:
+        random_page = wikipedia.random()
+        page_summary = wikipedia.summary(random_page, sentences=2)
+        start.say("Did you know? " + page_summary)
+        start.runAndWait()
+    except wikipedia.exceptions.DisambiguationError as e:
+        start.say("I found multiple options. Here are some suggestions: " + ', '.join(e.options))
+        start.runAndWait()
+    except wikipedia.exceptions.PageError:
+        start.say("I couldn't find any information right now.")
+        start.runAndWait()
 
 def hi(task):
-    if task.startswith(opts["alias"]):
-        start.say("всегда к вашим услугам сэр")
-        start.runAndWait()
+    if any(greeting in task for greeting in opts["alias"]):
+        regular_speak_greeting()
 
-    if task.startswith(opts["ctime"]):
+    elif any(question in task for question in opts["alias"]):
+        regular_speak_response()
+
+    elif any(how_are_you in task for how_are_you in opts["how_are_you"]):
+        respond_to_how_are_you()
+
+    elif any(new_project in task for new_project in ["start a new project", "new project", "begin a project"]):
+        respond_to_new_project()
+
+    elif any(help_command in task for help_command in ["help me", "need help", "assist me"]):
+        respond_to_need_help()
+
+    elif any(website_command in task for website_command in ["open"]):
+        url_to_open = task.replace("open", "").strip()
+        open_website(url_to_open)
+
+    elif task.startswith(opts["ctime"]):
         now = datetime.datetime.now()
-        start.say("Сейчас " + str(now.hour) + "часов" + str(now.minute) + "минут")
-        start.runAndWait()
-    if task.startswith(opts["note"]):
-        os.system(' start notepad')
-        start.say(random.choice(finish))
-        start.runAndWait()
+        custom_log("It's " + str(now.hour) + " o'clock and " + str(now.minute) + " minutes")
+
+    elif task.startswith(opts["note"]):
+        os.system('start notepad')
+        custom_log(random.choice(finish))
     if task.startswith(opts["bin"]):
         bin()
         start.say(random.choice(finish))
@@ -87,10 +160,12 @@ def hi(task):
         start.say(random.choice(finish))
         start.runAndWait()
     if task.startswith(opts["bye"]):
-        start.say("пока сэр")
+        start.say("Goodbye, sir")
         start.runAndWait()
         sleep(0.1)
         exit()
+    if task.startswith(opts["tell"]):
+        tell_me_something()
     if task.startswith(opts["google"]):
         webbrowser.open(task)
         start.say(random.choice(finish))
@@ -100,20 +175,23 @@ def hi(task):
         start.say(random.choice(finish))
         start.runAndWait()
     if task.startswith(opts["thank"]):
-        start.say("всё что угодно")
+        start.say("You're welcome")
         start.runAndWait()
-
 
 def bin():
     try:
         winshell.recycle_bin().empty(confirm=False, show_progress=False, sound=True)
-        start.say("корзина очищена")
+        start.say("Recycle bin cleared")
         start.runAndWait()
     except:
-        start.say("корзина уже была очищена")
+        start.say("The recycle bin was already empty")
         start.runAndWait()
+
 def request(task):
     hi(task)
 
 while True:
     request(listen())
+
+
+
